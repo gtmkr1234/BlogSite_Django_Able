57bc52756c91d7cf180ccb743db2d26845bd152a
gtmkr1234 <gtmkr1234@gmail.com>
2023-06-20 06:03:28 +0530

    added

--- blogSite/blogSite/views.py
+++ blogSite/blogSite/views.py
@@ -1,6 +1,19 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 
 def home(request):
     return render(request,"index.html")
+
+
+from django.contrib.auth.forms import UserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
